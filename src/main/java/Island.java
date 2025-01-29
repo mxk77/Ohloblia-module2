e93bff068a20ea10a8d65ac6507d8e2f1d44eef0
maxog <maxogloblia@gmail.com>
2025-01-29 02:01:02 +0200

    основа для майбутньої реалізації переміщення

---
+++ src/main/java/Island.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Island {
+    private final List<List<Location>> map = new ArrayList<>();
+
+    private static final int WIDTH = 100;
+    private static final int HEIGHT = 20;
+
+    public Island() {
+        for (int x = 0; x < WIDTH; x++) {
+            List<Location> column = new ArrayList<>();
+            for (int y = 0; y < HEIGHT; y++) {
+                column.add(new Location(x, y));
+            }
+            map.add(column);
+        }
+    }
+
+    public Location getLocation(int x, int y) {
+        return map.get(x).get(y);
+    }
+}
---
+++ src/main/java/Location.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Location {
+    private final int x;
+    private final int y;
+
+    private final List<Animal> animals = new ArrayList<>();
+    private final List<Plant> plants = new ArrayList<>();
+
+    public Location(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public List<Animal> getAnimals() {
+        return animals;
+    }
+
+    public List<Plant> getPlants() {
+        return plants;
+    }
+
+    public void addAnimal(Animal animal) {
+        animals.add(animal);
+    }
+
+    public void removeAnimal(Animal animal) {
+        animals.remove(animal);
+    }
+
+    public void addPlant(Plant plant) {
+        plants.add(plant);
+    }
+
+    public void removePlant(Plant plant) {
+        plants.remove(plant);
+    }
+}
